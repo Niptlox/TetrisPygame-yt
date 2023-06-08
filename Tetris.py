1e17401ea2fab1b54daadbb6f793fdac1ed3efe7
Niptlox <ggnipt@ya.ru>
2023-06-08 16:48:30 +0500

    add 2

--- Tetris.py
+++ Tetris.py
@@ -1,166 +1,178 @@
-from collections import defaultdict
-
-import pygame as pg
 import random
 
+import pygame as pg
+
 pg.init()
-pg.display.set_caption("TETRIS.py")
-FPS = 60
-WSIZE = (440, 500)
-TSIDE = 25
-MSIZE = (10, 20)
+# tile size
+TSIZE = 25
+# screen size
+WSIZE = (TSIZE * 18, TSIZE * 20)
+FPS = 30
+# field size
+FSIZE = (10, 20)
 
-font = pg.font.SysFont("", 40)
-
-
-def rotate_right90(points):
-    # x, y => -y, x
-    return [(-p[1], p[0]) for p in points]
-
-
-def new_figure():
-    global player_figure, player_color, next_figure, next_color, pos, game_over
-    player_figure = next_figure[:]
-    player_color = next_color
-    next_figure = random.choice(figures)
-    next_color = random.choice(colors)
-    pos = [5, 1]
-
-    if check_collide():
-        game_over = True
-
-
-def check_collide():
-    for point in player_figure:
-        x, y = pos[0] + point[0], pos[1] + point[1]
-        if (x, y) in field or not (0 <= x < MSIZE[0] and 0 <= y < MSIZE[1]):
-            return True
-    return False
-
-
-def del_line(iy):
-    global field
-    new_field = {}
-    for p, color, in field.items():
-        if p[1] > iy:
-            new_field[p] = color
-        if p[1] < iy:
-            new_field[(p[0], p[1] + 1)] = color
-    field = new_field
-
+font1 = pg.font.SysFont('Roboto', 40)
+font2 = pg.font.SysFont('Roboto', 42, bold=True)
+font3 = pg.font.SysFont('Roboto', 45, bold=True)
 
 screen = pg.display.set_mode(WSIZE)
 clock = pg.time.Clock()
+pg.display.set_caption("TETRIS v0")
 
-colors = ("red", "green", "orange", "blue", "violet", "yellow")
-field = {}
-line_color = "#27272A"
-
-figures = [((-1, 0), (0, 0), (1, 0), (1, 1)),  # Г
-           ((-1, 0), (0, 0), (1, 0), (1, -1)),  # Г 2
-           ((-1, 0), (0, 0), (1, 0), (0, -1)),  # 3
-           ((-1, 0), (0, 0), (1, 1), (0, 1)),  #
-           ((-1, 1), (0, 0), (1, 0), (0, 1)),
-           ((-1, 0), (0, 0), (1, 0), (2, 0)),
-           ]
-
-game_over = False
-speed_boost = False
-timer = 0
-best_score = score = 0
-
-global player_figure, player_color, pos
-next_figure = random.choice(figures)
-next_color = random.choice(colors)
-new_figure()
-
+figures = {
+    "I": ((-1, 0), (0, 0), (1, 0), (2, 0)),
+    "J": ((-1, -1), (-1, 0), (0, 0), (1, 0)),
+    "L": ((-1, 0), (0, 0), (1, -1), (1, 0)),
+    "O": ((0, -1), (0, 0), (1, -1), (1, 0)),
+    "S": ((-1, 0), (0, -1), (0, 0), (1, -1)),
+    "T": ((-1, 0), (0, -1), (0, 0), (1, 0)),
+    "Z": ((-1, -1), (0, -1), (0, 0), (1, 0))
+}
+figure_rotations_I = {0: ((-1, 0), (0, 0), (1, 0), (2, 0)),
+                      90: ((1, -1), (1, 0), (1, 1), (1, 2)),
+                      180: ((-1, 1), (0, 1), (1, 1), (2, 1)),
+                      270: ((0, -1), (0, 0), (0, 1), (0, 2)), }
+colors = ("cyan", "blue", "orange", "yellow", "green", "purple", "red")
 running = True
+field = [[None] * FSIZE[0] for _ in range(FSIZE[1])]
+
+now_pos = [4, 0]
+angle = 0
+now_color = random.choice(colors)
+now_figure = random.choice(list(figures.items()))
+next_color = random.choice(colors)
+next_figure = random.choice(list(figures.items()))
+speedup = False
+
+grav_time = 5
+grav_timer = 0
+
+score = 0
+high_score = 0
+
+
+def collide(field, figure, pos):
+    for px, py in figure[1]:
+        x, y = pos[0] + px, pos[1] + py
+        if x < 0 or x >= FSIZE[0] or y >= FSIZE[1]:
+            return True
+        if y >= 0 and field[y][x] is not None:
+            return True
+
+
+def new_figure():
+    global now_figure, now_color, next_color, next_figure, now_pos, angle
+    now_figure = next_figure
+    now_color = next_color
+    next_figure = random.choice(list(figures.items()))
+    next_color = random.choice(colors)
+    now_pos = [4, 0]
+    angle = 0
+
+
+def rotate_right90(figure, current_angle):
+    if figure[0] == "O":
+        return figure, current_angle
+    current_angle = (current_angle + 90) % 360
+    if figure[0] == "I":
+        return (figure[0], figure_rotations_I[current_angle]), current_angle
+    return (figure[0], tuple((-y, x) for x, y in figure[1])), current_angle
+
+
+def check_line(field):
+    global score
+    for iy in range(FSIZE[1]):
+        if all(field[iy]):
+            score += 10
+            field.pop(iy)
+            field.insert(0, [None] * FSIZE[0])
+
+
+def game_over():
+    global field, next_color, next_figure, high_score, score, running
+    _running = True
+    rendered_text = font2.render(f"Press SPACE for restart", True, "white")
+    screen.blit(rendered_text, ((WSIZE[0] - rendered_text.get_width()) // 2, 250))
+    pg.display.flip()
+    while _running:
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                running = _running = False
+            if event.type == pg.KEYDOWN and event.key == pg.K_SPACE:
+                _running = False
+    high_score = max(high_score, score)
+    score = 0
+    field = [[None] * FSIZE[0] for _ in range(FSIZE[1])]
+    next_color = random.choice(colors)
+    next_figure = random.choice(list(figures.items()))
+    new_figure()
+
+
 while running:
     clock.tick(FPS)
-    screen.fill("black")
-
-    best_score = max(score, best_score)
-    text = font.render(f"TETRIS", True, "White")
-    screen.blit(text, (MSIZE[0] * TSIDE + 50, 40))
-    text = font.render(f"Score: {score}", True, "Yellow")
-    screen.blit(text, (MSIZE[0] * TSIDE + 15, WSIZE[1] - 100))
-    text = font.render(f"Best: {best_score}", True, "Yellow")
-    screen.blit(text, (MSIZE[0] * TSIDE + 15, WSIZE[1] - 60))
-
-    # обработка событий
+    screen.fill("#18181B")
+    field_w = FSIZE[0] * TSIZE
+    rendered_title = font3.render(f"TETRIS", True, "white")
+    rendered_score = font1.render(f"Score: {score}", True, "#FCD34D")
+    rendered_high = font1.render(f"High: {high_score}", True, "#FCD34D")
+    screen.blit(rendered_title, (field_w + 30, 20))
+    screen.blit(rendered_score, (field_w + 10, 390))
+    screen.blit(rendered_high, (field_w + 10, 430))
+    # update user events
     for event in pg.event.get():
         if event.type == pg.QUIT:
             running = False
-        if game_over:
-            if event.type == pg.KEYDOWN and event.key == pg.K_SPACE:
-                # restart
-                game_over = False
-                field = {}
-                score = 0
-                new_figure()
-                new_figure()
-        else:
-            if event.type == pg.KEYDOWN:
-                if event.key == pg.K_SPACE:
-                    speed_boost = True
-                elif event.key == pg.K_LEFT:
-                    pos[0] -= 1
-                    if check_collide():
-                        pos[0] += 1
-                elif event.key == pg.K_RIGHT:
-                    pos[0] += 1
-                    if check_collide():
-                        pos[0] -= 1
-                elif event.key == pg.K_UP:
-                    _player_figure = player_figure
-                    player_figure = rotate_right90(player_figure)
-                    if check_collide():
-                        player_figure = _player_figure
-
-    if not game_over:
-        # отрисовка игрока
-        for point in player_figure:
-            x, y = pos[0] + point[0], pos[1] + point[1]
-            pg.draw.rect(screen, player_color, (x * TSIDE, y * TSIDE, TSIDE, TSIDE))
-    # отрисовка след фигуры
-    for point in next_figure:
-        x, y = MSIZE[0] + 3.2 + point[0], 4 + point[1]
-        pg.draw.rect(screen, next_color, (x * TSIDE, y * TSIDE, TSIDE - 1, TSIDE - 1))
-    lines = defaultdict(int)
-    # отрисовка остальных фигур
-    for ixy, cell_color in field.items():
-        x, y = ixy
-        pg.draw.rect(screen, cell_color, (x * TSIDE, y * TSIDE, TSIDE, TSIDE))
-        lines[y] += 1
-    # удаление заполненных линий
-    for iy, c in lines.items():
-        if c == MSIZE[0]:
-            del_line(iy)
+        elif event.type == pg.KEYDOWN:
+            new_pos = list(now_pos)
+            _figure = now_figure
+            if event.key == pg.K_RIGHT:
+                new_pos[0] += 1
+            elif event.key == pg.K_LEFT:
+                new_pos[0] -= 1
+            elif event.key == pg.K_DOWN:
+                new_pos[1] += 1
+            elif event.key == pg.K_SPACE:
+                speedup = True
+            elif event.key == pg.K_UP:
+                _figure, angle = rotate_right90(now_figure, angle)
+            if not collide(field, _figure, new_pos):
+                now_pos = new_pos
+                now_figure = _figure
+    # draw field
+    for y in range(FSIZE[1]):
+        for x in range(FSIZE[0]):
+            if field[y][x]:
+                pg.draw.rect(screen, field[y][x], ((x * TSIZE, y * TSIZE), (TSIZE, TSIZE)))
+    # draw current figure
+    for point in now_figure[1]:
+        pg.draw.rect(screen, now_color,
+                     (((now_pos[0] + point[0]) * TSIZE, (now_pos[1] + point[1]) * TSIZE), (TSIZE, TSIZE)))
+    # draw next figure
+    for point in next_figure[1]:
+        pg.draw.rect(screen, next_color,
+                     (((FSIZE[0] + point[0] + 3) * TSIZE, (point[1] + 5) * TSIZE), (TSIZE, TSIZE)))
+    # draw grid
+    for y in range(FSIZE[1] + 1):
+        # (156, 163, 175, 150)
+        pg.draw.rect(screen, "#3F3F46", ((0, y * TSIZE), (FSIZE[0] * TSIZE, 1)))
+    for x in range(FSIZE[0] + 1):
+        pg.draw.rect(screen, "#3F3F46", ((x * TSIZE, 0), (1, FSIZE[1] * TSIZE)))
+    # update gravity and collide
+    if grav_timer <= 0 or speedup:
+        if speedup:
+            score += 1
+        grav_timer = grav_time
+        now_pos[1] += 1
+        if collide(field, now_figure, now_pos):
             score += 10
-    # отрисовка сетки
-    for ix in range(MSIZE[0] + 1):
-        pg.draw.line(screen, line_color, (ix * TSIDE, 0), (ix * TSIDE, MSIZE[1] * TSIDE), 1)
-    for iy in range(MSIZE[1] + 1):
-        pg.draw.line(screen, line_color, (0, iy * TSIDE), (MSIZE[0] * TSIDE, iy * TSIDE), 1)
-
-    if game_over:
-        text = font.render("GAME OVER", True, "black")
-        screen.blit(text, ((WSIZE[0] - text.get_width()) // 2, WSIZE[1] // 2))
-        text = font.render("GAME OVER", True, "white")
-        screen.blit(text, (2+(WSIZE[0] - text.get_width()) // 2, WSIZE[1] // 2+2))
+            speedup = False
+            for px, py in now_figure[1]:
+                x, y = now_pos[0] + px, now_pos[1] + py - 1
+                field[y][x] = now_color
+            check_line(field)
+            new_figure()
+            if collide(field, now_figure, now_pos):
+                game_over()
     else:
-        # падение вниз
-        if timer <= 0 or speed_boost:
-            pos[1] += 1
-            if check_collide():
-                speed_boost = False
-                pos[1] -= 1
-                for point in player_figure:
-                    xy = pos[0] + point[0], pos[1] + point[1]
-                    field[xy] = player_color
-                new_figure()
-                score += 1
-
-            timer = 30
-        timer -= 1
+        grav_timer -= 1
     pg.display.flip()
